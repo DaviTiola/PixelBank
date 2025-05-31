3769b3706dd391cd98036e917f7c7834ab93509d
DaviTiola <davitiolafernandes@gmail.com>
2025-05-31 10:17:27 -0300

    Initial commit: Spring Boot project setup for PixelBank

---
+++ src/main/java/br/com/davidev/pixelbank/PixelBankApplication.java
@@ -0,0 +1,13 @@
+package br.com.davidev.pixelbank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PixelBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PixelBankApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/davidev/pixelbank/PixelBankApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.davidev.pixelbank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PixelBankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
