341ba84951532573a315183c6907e11989e49f65
DaviTiola <davitiolafernandes@gmail.com>
2025-06-03 08:59:51 -0300

    feat(security): Configure PasswordEncoder bean and inject into GamerUserService

--- src/main/java/br/com/davidev/pixelbank/service/GamerUserServiceImpl.java
+++ src/main/java/br/com/davidev/pixelbank/service/GamerUserServiceImpl.java
@@ -4,13 +4,17 @@ import br.com.davidev.pixelbank.dto.GamerUserCreateRequestDTO;
 import br.com.davidev.pixelbank.dto.GamerUserResponseDTO;
 import br.com.davidev.pixelbank.repository.GamerRepository;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.time.LocalDateTime;
 
 public class GamerUserServiceImpl implements GamerUserService{
 
     private final GamerRepository gamerRepository;
 
-    public GamerUserServiceImpl (GamerRepository gamerRepository) {
+    public GamerUserServiceImpl(GamerRepository gamerRepository, PasswordEncoder passwordEncoder) {
         this.gamerRepository = gamerRepository;
+        this.passwordEncoder = passwordEncoder; // Atribuir
     }
 
     @Override
@@ -18,10 +22,15 @@ public class GamerUserServiceImpl implements GamerUserService{
     public GamerUserResponseDTO createUser (GamerUserCreateRequestDTO createRequestDTO) {
         if (createRequestDTO == null) throw new IllegalArgumentException("RequestDTO não pode ser nulo");
         GamerUserResponseDTO response = new GamerUserResponseDTO();
+
         response.setId(1L);
         response.setUsername(createRequestDTO.getUsername());
+        response.setEmail(createRequestDTO.getEmail());
+        response.setXpPoints(0L);
+        response.setLevel(1);
+        response.setCreatedAt(LocalDateTime.now());
 
-        return null;
+        return response;
     }
 
 }
